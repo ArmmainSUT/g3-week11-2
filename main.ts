64ed8555be52bef0ec6f759afd2ac454c87623fe
Apichat Seeda <armmainsut@gmail.com>
2020-10-15 22:54:43 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    for (let index = 0; index <= 4; index++) {
+        led.plot(index, randint(0, 4))
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (led.point(0, 0)) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
+})
+basic.forever(function () {
+	
+})
